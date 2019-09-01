7a45aec1faa7f6133cd25ffa6c8a58c1aaa81742
Swapnil Patil <swapnil.ee.30@gmail.com>
2019-08-31 17:30:23 -0700

    Updates

---
+++ precache-manifest.7f99d021e7982242d6b94a9ccd54fee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56c2ef694237ba8ac58e22c9bc36d1f",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c68df287c40a2434e70a",
+    "url": "/react-tic-tac-toe/static/css/main.fd601fb9.chunk.css"
+  },
+  {
+    "revision": "a9071342250aba410f9a",
+    "url": "/react-tic-tac-toe/static/js/2.ff467aff.chunk.js"
+  },
+  {
+    "revision": "c68df287c40a2434e70a",
+    "url": "/react-tic-tac-toe/static/js/main.da84c3ba.chunk.js"
+  },
+  {
+    "revision": "94552da2264ad742ec31",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.0adcbbc0.js"
+  }
+]);
